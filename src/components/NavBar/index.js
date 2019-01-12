c152a944d242f392e3199477b2d4df5aadb76278
Gurgen Hayrapetyan <info.gurgen@gmail.com>
2019-01-12 14:50:39 +0400

    feat(navigation): add NavBar + MUI

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Menu from '@material-ui/core/Menu';
+import Avatar from '@material-ui/core/Avatar';
+import AppBar from '@material-ui/core/AppBar';
+import MenuIcon from '@material-ui/icons/Menu';
+import Toolbar from '@material-ui/core/Toolbar';
+import MenuItem from '@material-ui/core/MenuItem';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import { withStyles } from '@material-ui/core/styles';
+
+import { auth } from '../../config/firebase';
+
+const styles = {
+  root: {
+    flexGrow: 1,
+  },
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
+};
+
+class NavBar extends React.Component {
+  static propTypes = {
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+  };
+
+  state = {
+    anchorEl: null,
+  };
+
+  handleMenu = event => {
+    this.setState({ anchorEl: event.currentTarget });
+  };
+
+  handleClose = () => {
+    this.setState({ anchorEl: null });
+  };
+
+  handleSignOut = () => {
+    auth.signOut();
+    this.handleClose();
+  }
+
+  renderAuthBar = (open, anchorEl, user) => (
+    <div>
+      <IconButton
+        aria-owns={open ? 'menu-appbar' : undefined}
+        aria-haspopup="true"
+        onClick={this.handleMenu}
+        color="inherit"
+      >
+        <Avatar alt="Remy Sharp" src={user.photoURL} />
+      </IconButton>
+      <Menu
+        id="menu-appbar"
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'right',
+        }}
+        open={open}
+        onClose={this.handleClose}
+      >
+        <MenuItem onClick={this.handleClose}>Profile</MenuItem>
+        <MenuItem onClick={this.handleSignOut}>Sign Out</MenuItem>
+      </Menu>
+    </div>
+  )
+
+  render() {
+    const { anchorEl } = this.state;
+    const { user, classes } = this.props;
+    const open = Boolean(anchorEl);
+
+    return (
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <IconButton className={classes.menuButton} color="inherit" aria-label="Menu">
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" color="inherit" className={classes.grow}>
+              Reddit Clone
+            </Typography>
+            {this.renderAuthBar(open, anchorEl, user)}
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default withStyles(styles)(NavBar);
--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,39 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Switch, Route, Link } from 'react-router-dom';
-
-import { auth } from '../../config/firebase';
+import { Switch, Route } from 'react-router-dom';
+import NavBar from '../../components/NavBar';
 import Posts from '../../containers/Posts';
 import AddPost from '../../containers/AddPost';
 
 class App extends React.Component {
 propTypes = {
-    user: PropTypes.object
+    user: PropTypes.object.isRequired,
   };
 
-  whatToRender = (Component, props={}) => <Component {...this.props} />
+  whatToRender = Component => <Component {...this.props} />
 
   render() {
     const { user } = this.props;
     
     return (
       <div className="App">
-        <h1>Reddit Clone</h1>
-        <div>
-        <p>user: {user.displayName}, email: {user.email}</p>
-        <Link to="/">Home</Link>
-        <br/>
-        <Link to="/posts">Posts</Link>
-        <br/>
-        <Link to="/add-post">Add a Post</Link>
-        <br/>
-        <button onClick={() => auth.signOut()}>LogOut</button>
-
+        <NavBar {...{ user }} />
         <Switch>
           <Route path="/posts" render={() => this.whatToRender(Posts)} />
           <Route path="/add-post" render={() => this.whatToRender(AddPost)} />
         </Switch>
-        </div>
       </div>
     );
   }
